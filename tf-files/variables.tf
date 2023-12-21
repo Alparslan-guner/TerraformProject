cbb530b864aaa66d0afa7bb5ea9576f1450aa34d
Alparslan-guner <alpi123321147@gmail.com>
2023-12-21 12:05:08 +0300

    added

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "alp"
+}
+
+variable "git-token" {
+  default = "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"
+}
+
+variable "key-name" {
+  default = "alparslan-yeni-pair"
+}
+
+variable "hosted-zone" {
+  default = "alparslan.com"
+}
